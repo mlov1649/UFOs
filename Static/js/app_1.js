10e5d01b80e1c7ae7d720b88568f26c0155e607b
Mitchell  Lovell <mitchell@mitchells-imac.mynetworksettings.com>
2022-07-28 04:40:51 -0400

    Challenge complete

--- Static/js/app_1.js
+++ Static/js/app_1.js
@@ -34,7 +34,7 @@ for (var i = 0; i < vegetables.length; i++) {
 
 
 
-// Module 11.5.1 Starts here
+// Module 11.5. Starts here
 
 function buildTable(data) {
   // First, clear out any existing data
@@ -56,3 +56,29 @@ function buildTable(data) {
   });
 }
 
+
+// Add Filters and 'If' Statement
+function handleClick() {
+  // Grab the datetime value from the filter
+  let date = d3.select("#datetime").property("value");
+  let filteredData = tableData;
+
+   // Check to see if a date was entered and filter the
+  // data using that date.
+  if (date) {
+    // Apply `filter` to the table data to only keep the
+    // rows where the `datetime` value matches the filter value
+    filteredData = filteredData.filter(row => row.datetime === date);
+  }
+
+   // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
